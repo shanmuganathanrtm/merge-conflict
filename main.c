7bccf14581c0d6ce3bb8455d8f8411c5cad2661c
Shanmuganathan RTM <shanmugampkm@gmail.com>
2020-09-27 01:36:52 -0700

    Added Feature1

--- main.c
+++ main.c
@@ -5,11 +5,19 @@ Initial Function()
   //Sample Code
 }
 
+
 Initial Security()
 {
  //Sample Code
 }
 
 
+feaature2()
+{
+  //Sample Code for Feature 2
+}
 
+feature1()
+{
+ //sample code for Feature 1
 }
